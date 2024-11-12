bd79292585eab815154fa3636a02a6689a7c63e8
Ade Rahmat Maulana <adermaulana15@gmail.com>
2024-11-12 13:01:08 +0800

    update

--- index.php
+++ index.php
@@ -12,21 +12,29 @@
     if (isset($_POST['login'])) {
         $username = $_POST['username'];
         $password = md5($_POST['password']);
-    
-        $login = mysqli_query($koneksi, "SELECT * FROM `admin_221043`
-                                    WHERE `username_221043` = '$username'
-                                    AND `password_221043` = '$password'");
-        $cek = mysqli_num_rows($login);
+        $role = $_POST['role'];
 
-        $loginOrangtua = mysqli_query($koneksi, "SELECT * FROM `orangtua_221043`
-                                    WHERE `username_221043` = '$username'
-                                    AND `password_221043` = '$password'");
-        $cekOrangtua = mysqli_num_rows($loginOrangtua);
 
-        $loginSiswa = mysqli_query($koneksi, "SELECT * FROM `siswa_221043`
-                                    WHERE `username_221043` = '$username'
-                                    AND `password_221043` = '$password'");
-        $cekSiswa = mysqli_num_rows($loginSiswa);
+        if($role == 'admin'){
+          $login = mysqli_query($koneksi, "SELECT * FROM `admin_221043`
+          WHERE `username_221043` = '$username'
+          AND `password_221043` = '$password'");
+          $cek = mysqli_num_rows($login);
+        }
+
+        if($role == 'orangtua'){
+          $loginOrangtua = mysqli_query($koneksi, "SELECT * FROM `orangtua_221043`
+          WHERE `username_221043` = '$username'
+          AND `password_221043` = '$password'");
+          $cekOrangtua = mysqli_num_rows($loginOrangtua);
+        }
+
+        if($role == 'siswa'){
+          $loginSiswa = mysqli_query($koneksi, "SELECT * FROM `siswa_221043`
+          WHERE `username_221043` = '$username'
+          AND `password_221043` = '$password'");
+          $cekSiswa = mysqli_num_rows($loginSiswa);
+        }
     
         if ($cek > 0) {
             // Ambil data user
@@ -70,144 +78,107 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Login</title>
-  <link rel="stylesheet" href="style.css" />
-  <!-- Font Awesome CDN link for icons -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" />
-
   <style>
-    /* Importing Google Fonts - Poppins */
-    @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap");
-
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap');
+    
     * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    font-family: "Poppins", sans-serif;
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+      font-family: 'Poppins', sans-serif;
     }
-
+    
     body {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 100vh;
-    padding: 15px;
-    background: #6777ef;
-    overflow: hidden;
+      background: #4c60da;
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+      justify-content: center;
     }
-
+    
     .wrapper {
-    max-width: 500px;
-    width: 100%;
-    background: #fff;
-    border-radius: 5px;
-    box-shadow: 0px 4px 10px 1px rgba(0, 0, 0, 0.1);
+      width: 380px;
+      background: #fff;
+      border-radius: 5px;
+      padding: 40px 30px;
+      box-shadow: 0px 10px 15px rgba(0,0,0,0.1);
     }
-
+    
     .wrapper .title {
-    height: 120px;
-    background: #4c60da;
-    border-radius: 5px 5px 0 0;
-    color: #fff;
-    font-size: 30px;
-    font-weight: 600;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+      margin-bottom: 30px;
+      text-align: center;
     }
-
-    .wrapper form {
-    padding: 25px 35px;
+    
+    .wrapper .title span {
+      font-size: 26px;
+      font-weight: 600;
+      color: #4c60da;
     }
-
+    
     .wrapper form .row {
-    height: 60px;
-    margin-top: 15px;
-    position: relative;
+      height: 45px;
+      margin-bottom: 20px;
+      position: relative;
     }
-
-    .wrapper form .row input {
-    height: 100%;
-    width: 100%;
-    outline: none;
-    padding-left: 70px;
-    border-radius: 5px;
-    border: 1px solid lightgrey;
-    font-size: 18px;
-    transition: all 0.3s ease;
+    
+    .wrapper form .row input,
+    .wrapper form .row select {
+      height: 100%;
+      width: 100%;
+      padding: 0 35px;
+      outline: none;
+      font-size: 16px;
+      border-radius: 5px;
+      border: 1px solid #ccc;
+      transition: all 0.3s ease;
     }
-
-    form .row input:focus {
-    border-color: #4c60da;
+    
+    .wrapper form .row select {
+      padding-left: 35px;
+      cursor: pointer;
+      appearance: none;
+      -webkit-appearance: none;
+      -moz-appearance: none;
+      background-image: url("data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='none' stroke='%23888' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'%3e%3cpolyline points='6 9 12 15 18 9'%3e%3c/polyline%3e%3c/svg%3e");
+      background-repeat: no-repeat;
+      background-position: right 10px center;
+      background-size: 15px;
     }
-
-    form .row input::placeholder {
-    color: #999;
-    }
-
+    
     .wrapper form .row i {
-    position: absolute;
-    width: 55px;
-    height: 100%;
-    color: #fff;
-    font-size: 22px;
-    background: #4c60da;
-    border: 1px solid #4c60da;
-    border-radius: 5px 0 0 5px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+      position: absolute;
+      width: 47px;
+      height: 100%;
+      font-size: 18px;
+      color: #888;
+      line-height: 45px;
+      text-align: center;
     }
-
-    .wrapper form .pass {
-    margin-top: 12px;
+    
+    .wrapper form .row input:focus,
+    .wrapper form .row select:focus {
+      border-color: #4c60da;
+      box-shadow: inset 0px 0px 2px 2px rgba(26,188,156,0.25);
     }
-
-    .wrapper form .pass a {
-    color: #4c60da;
-    font-size: 17px;
-    text-decoration: none;
+    
+    .wrapper form .row .button input {
+      color: #fff;
+      font-size: 18px;
+      font-weight: 500;
+      padding: 0;
+      background: #1abc9c;
+      border: 1px solid #1abc9c;
+      cursor: pointer;
     }
-
-    .wrapper form .pass a:hover {
-    text-decoration: underline;
-    }
-
-    .wrapper form .button input {
-    margin-top: 20px;
-    color: #fff;
-    font-size: 20px;
-    font-weight: 500;
-    padding-left: 0px;
-    background: #4c60da;
-    border: 1px solid #4c60da;
-    cursor: pointer;
-    }
-
-    form .button input:hover {
-    background: #6777ef;
-    }
-
-    .wrapper form .signup-link {
-    text-align: center;
-    margin-top: 45px;
-    font-size: 17px;
-    }
-
-    .wrapper form .signup-link a {
-    color: #16a085;
-    text-decoration: none;
-    }
-
-    form .signup-link a:hover {
-    text-decoration: underline;
+    
+    .wrapper form .row .button input:hover {
+      background: #16a085;
     }
   </style>
-
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
 <body>
   <div class="wrapper">
@@ -215,16 +186,24 @@
     <form method="POST">
       <div class="row">
         <i class="fas fa-user"></i>
-        <input type="text" placeholder="Username" name="username" required />
+        <input type="text" placeholder="Username" name="username" required>
       </div>
       <div class="row">
         <i class="fas fa-lock"></i>
-        <input type="password" placeholder="Password" name="password" required />
+        <input type="password" placeholder="Password" name="password" required>
+      </div>
+      <div class="row">
+        <i class="fas fa-user-tag"></i>
+        <select name="role" required>
+          <option value="" disabled selected>Select Role</option>
+          <option value="admin">Admin</option>
+          <option value="siswa">Siswa</option>
+          <option value="orangtua">Orang Tua</option>
+        </select>
       </div>
       <div class="row button">
-        <input type="submit" value="Login" name="login" />
+        <input type="submit" value="Login" name="login">
       </div>
-      <!-- <div class="signup-link">Not a member? <a href="#">Signup now</a></div> -->
     </form>
   </div>
 </body>
